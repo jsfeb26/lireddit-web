0393b31f0273784fb492e0c1e02d6dae25b42e8d
Jason Stinson <jsfeb26@gmail.com>
2022-01-18 16:06:40 -0600

    Create Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml="auto">
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </Box>
+    </Flex>
+  );
+};
