fb462c86a5d542db9c52fee1173e2f606d20e84d
Jason Stinson <jsfeb26@gmail.com>
2022-01-20 16:10:27 -0600

    Max NavBar sticky

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -42,7 +42,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   }
 
   return (
-    <Flex bg="blue.600" p={4}>
+    <Flex position="sticky" top={0} zIndex={1} bg="blue.600" p={4}>
       <Box ml="auto">{body}</Box>
     </Flex>
   );
