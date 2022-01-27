f5c19ccf9c82230dd359860701730f76318d8f54
Jason Stinson <jsfeb26@gmail.com>
2022-01-27 11:52:13 -0600

    Show erorr message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,12 +22,17 @@ const Index = () => {
     limit: 15,
     cursor: null as string | null,
   });
-  const [{ data, fetching }] = usePostsQuery({
+  const [{ data, error, fetching }] = usePostsQuery({
     variables,
   });
 
   if (!fetching && !data) {
-    return <div>No posts</div>;
+    return (
+      <div>
+        <div>No posts</div>
+        <div>{error?.message}</div>
+      </div>
+    );
   }
 
   return (
